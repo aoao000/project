9fa81f6f9b918335a8503f6b0f006bf7be511b60
aoao000 <80384188+aoao000@users.noreply.github.com>
2021-06-29 00:12:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+	$('#BackTop').click(function(){ 
+		$('html,body').animate({scrollTop:0}, 333);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 300 ){
+			$('#BackTop').fadeIn(222);
+		} else {
+			$('#BackTop').stop().fadeOut(222);
+		}
+	}).scroll();
+});
+
+
+
+function more(){ 
+  document.getElementById('other').style.display="block";
+}
